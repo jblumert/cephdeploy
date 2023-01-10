f745c2a2522caa2d3d0fc2eae518f39578327455
jblumert <66739278+jblumert@users.noreply.github.com>
2023-01-10 17:45:08 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "cephadm bootstrap --apply-spec spec.yaml --mon-ip 192.168.252.11 --ssh-private-key /root/.ssh/id_rsa --ssh-public-key /root/.ssh/id_rsa.pub --registry-url registry.redhat.io --registry-username blumert --registry-password Washt0ning*1961"
-cephadm bootstrap --apply-spec spec.yaml --mon-ip 192.168.252.11 --ssh-private-key /root/.ssh/id_rsa --ssh-public-key /root/.ssh/id_rsa.pub --registry-url registry.redhat.io --registry-username blumert --registry-password Washt0ning*1961
+echo "cephadm bootstrap --apply-spec spec.yaml --mon-ip 192.168.252.11 --ssh-private-key /root/.ssh/id_rsa --ssh-public-key /root/.ssh/id_rsa.pub --registry-url registry.redhat.io --registry-username <userid> --registry-password <pwd>"
+cephadm bootstrap --apply-spec spec.yaml --mon-ip 192.168.252.11 --ssh-private-key /root/.ssh/id_rsa --ssh-public-key /root/.ssh/id_rsa.pub --registry-url registry.redhat.io --registry-username <userid> --registry-password <pwd>
 
